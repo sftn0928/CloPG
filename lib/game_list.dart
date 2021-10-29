e1fff9e140ebb698ef8f65a815db8386bb66dd49
saito yuto <57883503+mercury-pluto1210@users.noreply.github.com>
2021-10-29 16:27:29 +0900

    クラス分けとテストデータの追加

--- lib/game_list.dart
+++ lib/game_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
 
 import 'game_choice.dart';
+import 'test_data.dart';
 
 // ゲーム一覧画面
 class GameList extends StatefulWidget {
@@ -10,30 +11,19 @@ class GameList extends StatefulWidget {
 }
 
 class _GameList extends State<GameList> {
-  List<String> gameTitle = [
-    'Apex',
-    'VALORANT',
-    'MONSTER HUNTER',
-  ];
-  List<String> gameDetail = [
-    'FPS',
-    'FPS',
-    'Action'
-  ];
+  // テストデータ呼び出し
+  Game game = Game();
 
-  // final firestore = FirebaseFirestore.instance;
-  // final doc = await firestore
-  //     .collection("tests")
-  //     .doc("xxxxxxxxxxxxxxxx")
-  //     .get();
-
+  // ゲームリストの表示
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Game List'),
       ),
       body: ListView.builder(
-          itemCount: gameTitle.length,
+          // テストのデータの要素数を取得
+          itemCount: game.index,
+          // その要素数分リストを作成する
           itemBuilder: (context, index){
             return Card(
               child: ListTile(
@@ -41,11 +31,11 @@ class _GameList extends State<GameList> {
                   width: 100,
                   color: Colors.red,
                 ),
-                title: Text(gameTitle[index]),
-                subtitle: Text(gameDetail[index]),
+                title: Text(game.title[index]),
+                subtitle: Text(game.detail[index]),
                 trailing: Icon(Icons.more_vert),
                 onTap: (){
-                  print('onTap');
+                  print('test');
                 },
               ),
             );
@@ -54,38 +44,4 @@ class _GameList extends State<GameList> {
       ),
     );
   }
-
-// static List<Widget> listTiles = <Widget>[
-//   ListTile(
-//     leading: Container(
-//       width: 100,
-//       color: Colors.red,
-//     ),
-//     title: Text('ゲームタイトル'),
-//     subtitle: Text('ジャンル'),
-//     // trailing: Icon(Icons.more_vert),
-//     trailing: Icon(
-//       alreadySaved ? Icons.favorite : Icons.favorite_border,
-//       color: alreadySaved ? Colors.red : null,
-//     ),
-//   ),
-//   ListTile(
-//     leading: Container(
-//       width: 100,
-//       color: Colors.blue,
-//     ),
-//     title: Text('ゲームタイトル'),
-//     subtitle: Text('ジャンル'),
-//     trailing: Icon(Icons.more_vert),
-//   ),
-//   ListTile(
-//     leading: Container(
-//       width: 100,
-//       color: Colors.green,
-//     ),
-//     title: Text('ゲームタイトル'),
-//     subtitle: Text('ジャンル'),
-//     trailing: Icon(Icons.more_vert),
-//   )
-// ];
 }
