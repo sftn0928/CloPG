0b96100d74d31bcd55e59392523c6bdeb3fc5df3
unknown <18ec092@ms.dendai.ac.jp>
2021-11-22 19:15:43 +0900

    'ユーザーの名前とコメント表示'

--- lib/my_page.dart
+++ lib/my_page.dart
@@ -1,14 +1,60 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:share/share.dart';
 
+class NetworkImageBuilder extends FutureBuilder {
+  NetworkImageBuilder(Future<String> item)
+      : item = item,
+        super(
+        future: item,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return CachedNetworkImage(
+              imageUrl: snapshot.data,
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      );
+  final Future<String> item;
+}
+
 class MyHomePage extends StatefulWidget {
+  // final String username;
+  // final String usercomment;
+  // final String userimg;
+  // final Stream<QuerySnapshot> _userstream =FirebaseFirestore.instance.collection('users').snapshots();
+  // MyHomePage(this.username,this.usercomment,this.userimg);
+  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
+
   final url = "https://ironodata.info/imghex2/0000FF.png";
+  Future GetUserinfo() async{
+    DocumentSnapshot snapshot = await FirebaseFirestore.instance.collection('users').doc('gn9O7UyS3wRsVM0WdHpVG6pA7Yh2').get();
+    // Map<String,dynamic>? username = user.data() as Map<String,dynamic>;
+    // return username['username'];
+    return snapshot.data();
+  }
+
+  // Future GetUserimg() async{
+  //   DocumentSnapshot snapshot = await FirebaseFirestore.instance.collection('users').doc('gn9O7UyS3wRsVM0WdHpVG6pA7Yh2').get();
+  //   return snapshot.data() as Map<String,dynamic>;
+  // }
+
+  // Future<void> downloadURLExample() async {
+  //   String downloadURL = await firebase_storage.FirebaseStorage.instance
+  //       .ref('users/123/avatar.jpg')
+  //       .getDownloadURL();
+  // }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,27 +79,55 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _myPageImage(){
+  Widget _myPageImage() {
     return Container(
       margin: EdgeInsets.all(30),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle, // Containerを丸くする
-                image: DecorationImage(
-                  fit: BoxFit.fill, // 親Widget(今回はContainer)と同じサイズになるように調整する
-                  image: NetworkImage(url),
-                ),
+        children: <Widget>[
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle, // Containerを丸くする
+              image: DecorationImage(
+                fit: BoxFit.fill, // 親Widget(今回はContainer)と同じサイズになるように調整する
+                image: NetworkImage(url),
               ),
-            )
-          ],
+            ),
+          )
+        ],
       ),
     );
   }
+
+
+  // Widget _myPageImage(){
+  //   return Container(
+  //     margin: EdgeInsets.all(30),
+  //     child: Row(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           FutureBuilder<dynamic>(
+  //             future: GetUserimg(),
+  //             builder: (context,snapshot) {
+  //               return Container(
+  //                 width: 150,
+  //                 height: 150,
+  //                 decoration: BoxDecoration(
+  //                   shape: BoxShape.circle, // Containerを丸くする
+  //                   image: DecorationImage(
+  //                     fit: BoxFit.fill, // 親Widget(今回はContainer)と同じサイズになるように調整する
+  //                     image: NetworkImageBuilder(.getDownloadURL()),
+  //                   ),
+  //                 ),
+  //               );
+  //             }
+  //           )
+  //         ],
+  //     ),
+  //   );
+  // }
 
   Widget _myProfileText(){
     return Container(
@@ -61,16 +135,28 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           Container(
             margin: const EdgeInsets.only(bottom: 20),
-            child: Text(
-              'None',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            child: FutureBuilder<dynamic>(
+              future: GetUserinfo(),
+              builder: (context,snapshot) {
+                return Text(
+                  snapshot.hasData ? '${snapshot.data['username']}' : 'No Name',
+                  //'None',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                );
+              }
             ),
           ),
           Container(
             margin: const EdgeInsets.only(bottom: 4),
-            child: Text(
-              '夏までに目指せ達成率100%!',
-              style: TextStyle(fontSize: 18),
+            child: FutureBuilder<dynamic>(
+                future: GetUserinfo(),
+              builder: (context,snapshot) {
+                return Text(
+                  snapshot.hasData ? '${snapshot.data['usercomment']}' : 'No Comment',
+                  //'夏までに目指せ達成率100%!',
+                  style: TextStyle(fontSize: 18),
+                );
+              }
             ),
           )
         ],
